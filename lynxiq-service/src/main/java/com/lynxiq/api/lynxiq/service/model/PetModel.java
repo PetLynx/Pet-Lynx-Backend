14e4f182754913a50afe9d8c6b705c2d709da728
USER <USER@DESKTOP-EQF2CA2>
2022-09-11 23:51:03 +0530

    issues

--- lynxiq-service/src/main/java/com/lynxiq/api/lynxiq/service/model/PetModel.java
+++ lynxiq-service/src/main/java/com/lynxiq/api/lynxiq/service/model/PetModel.java
@@ -1,12 +1,6 @@
 package com.lynxiq.api.lynxiq.service.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@Data
 public class PetModel {
 
     private String id;
@@ -15,5 +9,44 @@ public class PetModel {
     private String photo;
     private String breed;
     private String type;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getBreed() {
+		return breed;
+	}
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+    
+    
+    
 
 }
